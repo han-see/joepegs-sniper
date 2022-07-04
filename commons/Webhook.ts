a93a2278f8dfa0f4bbb1608a99c86980eaa1980f
han-see <74469036+han-see@users.noreply.github.com>
2022-07-04 23:02:32 +0200

    Add webhook connection

---
+++ commons/Webhook.ts
@@ -0,0 +1,28 @@
+import { MessageEmbed, WebhookClient } from "discord.js";
+
+/**
+ * This class is use to send a message to the user discord
+ */
+export class Webhook {
+    private embed: MessageEmbed;
+    private userWebhookClient = new WebhookClient({
+        url: process.env.USER_DISCORD_WEBHOOK!,
+    });
+
+    constructor(private title: string) {
+        this.embed = new MessageEmbed().setTitle(this.title).setTimestamp();
+    }
+
+    async sendMessageToUser(content: string, txhash?: string) {
+        this.embed.setDescription(content);
+        this.embed.setURL(`https://snowtrace.io/tx/${txhash}`);
+        await this.userWebhookClient
+            .send({
+                content: `<@${process.env.DISCORD_USER_ID}>\n${this.embed.title}`,
+                embeds: [this.embed],
+            })
+            .catch((err) => {
+                console.error("Cannot send message to discord", err);
+            });
+    }
+}
