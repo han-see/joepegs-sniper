d2a02784de5e3c3c400d7d84c1c050f08489780b
han-see <74469036+han-see@users.noreply.github.com>
2022-07-04 18:03:18 +0200

    initial config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -9,24 +9,31 @@ import "hardhat-deploy";
 import "solidity-coverage";
 import { HardhatUserConfig } from "hardhat/config";
 
-const FUJI_PRIVATE_KEY =
-  process.env.FUJI_PRIVATE_KEY ||
+const PRIVATE_KEY =
+  process.env.PRIVATE_KEY ||
   "0x11ee3108a03081fe260ecdc106554d09d9d1209bcafd46942b10e02943effc4a";
+const AVAX_RPC_URL = process.env.AVAX_RPC_URL || "test";
+const FUJI_RPC_URL = process.env.FUJI_RPC_URL || "test";
 
 const config: HardhatUserConfig = {
   defaultNetwork: "hardhat",
   networks: {
     hardhat: {
-      chainId: 31337,
       forking: {
-        url: "https://api.avax.network/ext/bc/C/rpc",
+        url: AVAX_RPC_URL,
       },
     },
     fuji: {
       chainId: 43113,
       gasPrice: 225000000000,
-      url: "https://api.avax-test.network/ext/bc/C/rpc",
-      accounts: [FUJI_PRIVATE_KEY],
+      url: FUJI_RPC_URL,
+      accounts: [PRIVATE_KEY],
+    },
+    avalanche: {
+      chainId: 43114,
+      gasPrice: 225000000000,
+      accounts: [PRIVATE_KEY],
+      url: AVAX_RPC_URL,
     },
   },
   solidity: {
