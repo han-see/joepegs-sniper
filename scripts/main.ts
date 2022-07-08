ad46b8084b4b2cae8c7ba13bccc8fc674254b219
han-see <74469036+han-see@users.noreply.github.com>
2022-07-08 18:37:40 +0200

    Change to buy the event

--- scripts/main.ts
+++ scripts/main.ts
@@ -21,8 +21,8 @@ function runBot() {
         listingContractWebhook,
         bots
     );
-    // eventListener.listenToEventFromRpcUrl();
-    eventListener.getLastFlatLaunchpeg();
+    eventListener.listenToEventFromRpcUrl();
+    //eventListener.getLastFlatLaunchpeg();
 }
 
 function initiateBot(): MintBot[] {
