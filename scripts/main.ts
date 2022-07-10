31eaab09738a3497339f214d56d9401e32f7e9f3
han-see <74469036+han-see@users.noreply.github.com>
2022-07-10 19:37:13 +0200

    Add multiple bots mint

--- scripts/main.ts
+++ scripts/main.ts
@@ -1,41 +1,40 @@
-import { Wallet } from "ethers";
-import { Webhook } from "../commons/Webhook";
-import { EventListener } from "./EventListener";
-import { JsonRpcProvider } from "@ethersproject/providers";
-import { MintBot } from "./Bot";
-require("dotenv").config();
+import { Wallet } from "ethers"
+import { Webhook } from "../commons/Webhook"
+import { EventListener } from "./EventListener"
+import { JsonRpcProvider } from "@ethersproject/providers"
+import { MintBot } from "./Bot"
+require("dotenv").config()
 
 function runBot() {
-    const isTest = process.argv[2] === "test" ? true : false;
-    const mainPK = isTest ? process.env.TEST_PK! : process.env.PRIVATE_KEY!;
+    const isTest = process.argv[2] === "test" ? true : false
+    const mainPK = isTest ? process.env.TEST_PK! : process.env.PRIVATE_KEY!
     const RPC_URL = isTest
         ? process.env.TEST_RPC!
-        : process.env.MORALIS_RPC_URL!;
-    const mainAccount: Wallet = new Wallet(
-        mainPK!,
-        new JsonRpcProvider(RPC_URL)
-    );
-    const listingContractWebhook = new Webhook("New listing");
-    const bots: MintBot[] = initiateBot(RPC_URL, mainPK);
+        : process.env.MORALIS_RPC_URL!
+    const listingContractWebhook = new Webhook("New listing")
+    const bots: MintBot[] = initiateBot(RPC_URL, JSON.parse(mainPK))
 
     const eventListener = new EventListener(
         RPC_URL,
         listingContractWebhook,
         bots
-    );
-    eventListener.listenToEventFromRpcUrl();
+    )
+    eventListener.listenToEventFromRpcUrl()
     // eventListener.getLastFlatLaunchpeg();
 }
 
-function initiateBot(rpcUrl: string, pk: string): MintBot[] {
-    //TODO change this
-    const bots: MintBot[] = [];
-    const account: Wallet = new Wallet(pk, new JsonRpcProvider(rpcUrl));
-    const webhook: Webhook = new Webhook("Bot 1");
-    const bot = new MintBot(account, webhook);
-    bots.push(bot);
-
-    return bots;
+function initiateBot(rpcUrl: string, privateKeys: string[]): MintBot[] {
+    const bots: MintBot[] = []
+    let i = 0
+    for (let pk of privateKeys) {
+        const account: Wallet = new Wallet(pk, new JsonRpcProvider(rpcUrl))
+        const webhook: Webhook = new Webhook(`Bot ${i}`)
+        const bot = new MintBot(account, webhook)
+        bots.push(bot)
+        console.log(`Bot ${i} initiated`)
+        i++
+    }
+    return bots
 }
 
-runBot();
+runBot()
