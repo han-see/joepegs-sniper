24fddeb2f085fa71160589ef8cd6349f999d5dbe
han-see <74469036+han-see@users.noreply.github.com>
2022-07-12 14:32:12 +0200

    Refactor manual bot into separate class

--- scripts/manual-bot.ts
+++ scripts/manual-bot.ts
@@ -9,16 +9,15 @@ function runBot() {
     const isTest = process.argv[2] === "test" ? true : false
     const mainPK = isTest ? process.env.TEST_PK! : process.env.PRIVATE_KEY!
     const RPC_URL = isTest ? process.env.TEST_RPC! : process.env.AVAX_RPC_URL!
-    const listingContractWebhook = new Webhook("New listing")
     const bots: MintBot[] = initiateBot(RPC_URL, JSON.parse(mainPK))
 
-    const eventListener = new EventListener(
-        RPC_URL,
-        listingContractWebhook,
-        bots
+    manualMint(
+        1657570800,
+        "0xcf0f4519F2f55Ef40e2dDDcb7c99893297E40336",
+        bots,
+        new JsonRpcProvider(RPC_URL),
+        new Webhook("Manual bot")
     )
-    eventListener.listenToEventFromRpcUrl()
-    // eventListener.getLastFlatLaunchpeg();
 }
 
 function initiateBot(rpcUrl: string, privateKeys: string[]): MintBot[] {
@@ -33,6 +32,32 @@ function initiateBot(rpcUrl: string, privateKeys: string[]): MintBot[] {
         i++
     }
     return bots
+}
+
+async function manualMint(
+    mintTime: number,
+    contractAddress: string,
+    bots: MintBot[],
+    provider: JsonRpcProvider,
+    webhook: Webhook
+) {
+    let counter = 1
+    for (const bot of bots) {
+        console.log(`Initiating bot ${counter}`)
+        bot.mintFreeFlatJoePeg(mintTime, contractAddress)
+        counter++
+    }
+
+    const blockNumber = await provider.getBlockNumber()
+    const blockTimestamp = (await provider.getBlock(blockNumber)).timestamp
+    if (blockTimestamp > mintTime) {
+        console.log("Mint time is in the past. Turning off manual minting.")
+        await webhook.sendMessageToUser(
+            `Manual minting time for ${contractAddress} is in the past. Turning off manual bot`
+        )
+        // to kill the process on manual minting
+        process.kill(process.pid, "SIGTERM")
+    }
 }
 
 runBot()
