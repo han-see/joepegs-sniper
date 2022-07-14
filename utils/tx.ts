3b79e8e9770493a076a783ea6cc429f81056815a
han-see <74469036+han-see@users.noreply.github.com>
2022-07-14 21:46:30 +0200

    Create a new tx function to create tx before event happens

---
+++ utils/tx.ts
@@ -0,0 +1,46 @@
+import { BigNumber, ethers, Wallet } from "ethers"
+import {
+    Interface,
+    parseEther,
+    parseUnits,
+    UnsignedTransaction,
+} from "ethers/lib/utils"
+
+export async function createSignedTx(
+    account: Wallet,
+    hexData: string,
+    contractAddress: string,
+    value: string
+): Promise<string> {
+    const walletNonce = await account.getTransactionCount()
+    const chainId = await account.getChainId()
+    const gasLimit = BigNumber.from(400000)
+    const maxFeePerGas = parseUnits("75", "gwei")
+    const maxPriorityFeePerGas = BigNumber.from(2)
+
+    const unSignedTx: UnsignedTransaction = {
+        to: contractAddress,
+        nonce: walletNonce,
+        gasLimit: gasLimit,
+        maxFeePerGas: maxFeePerGas,
+        maxPriorityFeePerGas: maxPriorityFeePerGas,
+        data: hexData,
+        value: parseEther(value),
+        chainId: chainId,
+        type: 2,
+    }
+
+    // @ts-ignore
+    const signedTx = await account.signTransaction(unSignedTx)
+
+    return signedTx
+}
+
+export async function createTxData(
+    abi: any,
+    functionCall: string,
+    args: any[]
+) {
+    const iface = new Interface(abi)
+    return iface.encodeFunctionData(functionCall, args)
+}
